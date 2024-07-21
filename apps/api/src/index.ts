b3a71684157f7ee12e6f6217fb7859d5c0fd71d4
Newton Duarte <newton_@hotmail.com>
2024-07-20 21:37:18 -0300

    feat: add auth permissions and user model

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,9 @@
-import { ability } from '@newton-saas/auth'
+import { defineAbilityFor } from '@newton-saas/auth'
+
+const ability = defineAbilityFor({ role: 'ADMIN' })
 
 const userCanInviteSomeoneElse = ability.can('invite', 'User')
 const userCanDeletSomeoneElse = ability.can('delete', 'User')
 
+console.log(userCanInviteSomeoneElse)
 console.log(userCanDeletSomeoneElse)
