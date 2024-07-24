1a52ea7f85098ba363eb9feed6be3e5fc71be39a
Newton Duarte <newton_@hotmail.com>
2024-07-24 11:53:25 -0300

    feat: add auto join organization check on create account

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -32,6 +32,15 @@ export async function createAccount(app: FastifyInstance) {
         return reply.status(400).send({ message: 'User already exists' })
       }
 
+      const [, domain] = email.split('@')
+
+      const autoJoinOrganization = await prisma.organization.findFirst({
+        where: {
+          domain,
+          shouldAttachUsersByDomain: true,
+        },
+      })
+
       const passwordHash = await hash(password, 6)
 
       await prisma.user.create({
@@ -39,6 +48,13 @@ export async function createAccount(app: FastifyInstance) {
           name,
           email,
           passwordHash,
+          member_on: autoJoinOrganization
+            ? {
+                create: {
+                  organizationId: autoJoinOrganization.id,
+                },
+              }
+            : undefined,
         },
       })
 
