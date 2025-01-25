3ae50071ba4a860b54e84c80cef8345519d79894
Newton Duarte <newton_@hotmail.com>
2025-01-25 10:42:36 -0300

    feat: register reject invite route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -21,7 +21,8 @@ import { resetPassword } from './routes/auth/reset-password'
 import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
-import { getInvites, getInvites } from './routes/invites/get-Invites'
+import { getInvites } from './routes/invites/get-Invites'
+import { rejectInvite } from './routes/invites/reject-invite'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -98,6 +99,7 @@ app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
 app.register(acceptInvite)
+app.register(rejectInvite)
 
 app
   .listen({
