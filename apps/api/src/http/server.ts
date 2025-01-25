a8a2ed44664a4b90e4aa20302ef0bd4b68df9f05
Newton Duarte <newton_@hotmail.com>
2025-01-25 10:39:57 -0300

    feat: register accept invite route

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,9 +18,10 @@ import { createAccount } from './routes/auth/create-account'
 import { getProfile } from './routes/auth/get-profile'
 import { requestPasswordRecover } from './routes/auth/request-password-recover'
 import { resetPassword } from './routes/auth/reset-password'
+import { acceptInvite } from './routes/invites/accept-invite'
 import { createInvite } from './routes/invites/create-invite'
 import { getInvite } from './routes/invites/get-invite'
-import { getInvites } from './routes/invites/get-Invites'
+import { getInvites, getInvites } from './routes/invites/get-Invites'
 import { getMembers } from './routes/members/get-members'
 import { removeMember } from './routes/members/remove-member'
 import { updateMember } from './routes/members/update-member'
@@ -96,6 +97,7 @@ app.register(removeMember)
 app.register(createInvite)
 app.register(getInvite)
 app.register(getInvites)
+app.register(acceptInvite)
 
 app
   .listen({
