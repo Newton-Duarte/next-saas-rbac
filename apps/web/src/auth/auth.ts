cf3aa39cfbb81aac94f05c29c8a0933ea69f201c
Newton Duarte <newton_@hotmail.com>
2025-02-01 08:09:09 -0300

    feat: add server-side authentication middleware function

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,8 +1,26 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 import { AUTH_TOKEN_KEY } from './constants'
 
 export async function isAuthenticated() {
   const nextjsCookies = await cookies()
   return !!nextjsCookies.get(AUTH_TOKEN_KEY)?.value
+}
+
+export async function auth() {
+  const nextjsCookies = await cookies()
+  const token = nextjsCookies.get(AUTH_TOKEN_KEY)?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+  try {
+    const { user } = await getProfile()
+    return { user }
+  } catch {}
+
+  redirect('/api/auth/sign-out')
 }
