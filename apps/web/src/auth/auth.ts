62156a8dcaf355537afc17f13d9c028e32687b1e
Newton Duarte <newton_@hotmail.com>
2025-02-03 09:58:15 -0300

    feat: add authentication and authorization utilities for current organization

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,6 +1,8 @@
+import { defineAbilityFor } from '@newton-saas/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
 
 import { AUTH_TOKEN_KEY } from './constants'
@@ -8,6 +10,38 @@ import { AUTH_TOKEN_KEY } from './constants'
 export async function isAuthenticated() {
   const nextjsCookies = await cookies()
   return !!nextjsCookies.get(AUTH_TOKEN_KEY)?.value
+}
+
+export async function getCurrentOrg() {
+  const nextjsCookies = await cookies()
+  return nextjsCookies.get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const org = await getCurrentOrg()
+
+  if (!org) {
+    return null
+  }
+
+  const { membership } = await getMembership(org)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
